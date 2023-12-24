bebf7fa15bb5c4474f98d5d9daa7095c708c71dd
Ayala Brown <Bayala@isrtech.co.il>
2023-12-24 16:27:48 +0200

    24/12/23

--- node.js/app.js
+++ node.js/app.js
@@ -1,9 +1,15 @@
 const tf = require('@tensorflow/tfjs');
 const { request } = require('express');
-const mssql = require('mssql');
-const plotly = require('plotly')('bAyala', '4DFS7mOSfInvya0GIawe');
-const { CubicSpline } = require('natural');
+// const mssql = require('mssql');
+const Plotly = require('plotly')('bAyala', '4DFS7mOSfInvya0GIawe');
+const { plot } = require('nodeplotlib');
+const Spline = require('cubic-spline');
 const ExcelJS = require('exceljs');
+const { normalize } = require('normalize');
+const { Builder, parseString  } = require('xml2js');
+const fs = require('fs');
+const path = require('path');
+const math = require('mathjs');
 
 // const server = '192.168.16.3';
 // const database = 'electric_ML';
@@ -21,176 +27,293 @@ const ExcelJS = require('exceljs');
 //   },
 // };
 
-const excelFilePath = './data.xlsx';
+// const connStr = `http://${username}:${password}@${server}:1433/${database}`;
 
-function fisherYatesShuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
+function calculateRSquared(yTrue, yPred) {
+    const yTrueMean = tf.mean(yTrue);
+    const totalSumOfSquares = tf.sum(tf.square(tf.sub(yTrue, yTrueMean)));
+    const residualSumOfSquares = tf.sum(tf.square(tf.sub(yTrue, yPred)));
+    const rSquared = tf.sub(1, tf.div(residualSumOfSquares, totalSumOfSquares));
+    return rSquared.dataSync()[0];
 }
 
-async function predictChargeTime(start_date, end_date) {
-
-  const workbook = new ExcelJS.Workbook();
-
-  try {
-    // await mssql.connect(config);
-    // const query = `exec dbo.GetElectricPointChargeDetails_acc '${start_date}','${end_date}';`;
-    // const result = await request.query(query);
-
+async function readData(excelFilePath) {
+    const workbook = new ExcelJS.Workbook();
     await workbook.xlsx.readFile(excelFilePath);
 
-    // Specify the worksheet you want to read from
-    const sheetName = 'Sheet1'; // Replace 'Sheet1' with the actual sheet name
+    const sheetName = 'Sheet1';
     const worksheet = workbook.getWorksheet(sheetName);
 
     if (!worksheet) {
-      throw new Error(`Worksheet '${sheetName}' not found in the workbook.`);
+        throw new Error(`Worksheet '${sheetName}' not found in the workbook.`);
     }
 
-    // Extract data from the worksheet into an array of objects
     const data = [];
+    let headers = null;
+
     worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
-      const rowData = {};
-      row.eachCell({ includeEmpty: false }, (cell, colNumber) => {
-        rowData[`Column${colNumber}`] = cell.value;
-      });
-      data.push(rowData);
+        const rowData = {};
+
+        if (!headers) {
+            headers = row.values.map(String);
+            return;
+        }
+
+        row.eachCell({ includeEmpty: false }, (cell, colNumber) => {
+            const header = headers[colNumber];
+            rowData[header] = cell.value;
+        });
+
+        data.push(rowData);
     });
-    
-    console.log(data);
 
-    // for (const bus of data.map((item) => item.Column1).filter((value, index, self) => self.indexOf(value) === index)) {
+    return data;
+}
 
-      bus="14:1F:BA:13:8E:F6";
 
-      // bus = "14:1F:BA:10:C6:18"
-      const filteredData = data.filter((row) => row.Column1 === bus);
-  
-      if (filteredData.length === 0) {
-        console.log(`No rows found with idtag ${bus}.`);
-        return;
-      }
-    
-      const X = tf.tensor2d(filteredData.map((row) => [row.Column4]), [filteredData.length, 1]);
-      const y = tf.tensor2d(filteredData.map((row) => [row.Column10]/60),  [filteredData.length, 1]);
-      const y_s = tf.tensor2d(filteredData.map((row) => [row.Column14]/60),  [filteredData.length, 1]);
-      
-      // const concatenatedData = tf.concat([X, y_s], 1);
+const excelFilePath = './data.xlsx';
 
-      // const concatenatedArray = Array.from(concatenatedData.dataSync());
+async function training(){
+    const data = await readData(excelFilePath);
 
-      // fisherYatesShuffle(concatenatedArray);
-      // console.log(concatenatedArray)
-      // const shuffledData = tf.tensor2d(concatenatedArray, concatenatedData.shape);
+    const uniqueIdtags = [...new Set(data.map(row => row.idtag))];
 
-      // const { X_train, X_test, y_train, y_test } = tf.split(shuffledData, [1, 1], 1);
+    // bus="14:1F:BA:13:8E:F6";
+    // bus = "14:1F:BA:10:C6:94";
+    bus = "14:1F:BA:10:7F:79"
 
-      // console.log(X_train, X_test, y_train, y_test)
-      // const scaler = tf.layers.normalization.batchNormalization();
-      // const model = tf.sequential({
-      //   layers: [
-      //     tf.layers.dense({ units: 100, activation: 'relu', inputShape: [2] }),
-      //     tf.layers.dense({ units: 50, activation: 'relu' }),
-      //     tf.layers.dense({ units: 25, activation: 'relu' }),
-      //     tf.layers.dense({ units: 1 }),
-      //   ],
-      // });
+    // levels:
+    // -140-170 : high
+    // -107-135: medium
+    // -95-106: low
+    // -70-90: very low
+    // -40-45: very very low
 
-      // model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
+    // charge = 290
+    const xmlData = {
+        buses: []
+    };
 
-      // const xs = scaler.apply(X_train);
-      // const ys = scaler.apply(y_train);
 
-      // await model.fit(xs, ys, { epochs: 100 });
 
-      // const x_predict_tensor = scaler.apply(tf.tensor(df1.map((item) => item.soc), [df1.length, 1]));
-      // const y_pred = model.predict(x_predict_tensor).arraySync();
+    // for (const bus of uniqueIdtags) {
 
-      // const data_combine = df1.map((item, index) => [item.soc, y_pred[index][0]]);
-      // const unique_x_dict = {};
-      // const filtered_combined = [];
+        const filteredD = data.filter((row) => row.idtag === bus);
+        const filteredData = filteredD.filter((row) => row.epcRowId === charge);
+        const _x = tf.tensor2d(filteredData.map((row) => [row.soc]), [filteredData.length, 1]).arraySync();
+        const _y = tf.tensor2d(filteredData.map((row) => [row.scaled]/60), [filteredData.length, 1]).arraySync();
+        console.log(_x, _y);
+        plotGraph(_x, _y);
 
-      // for (const [x_val, y_val] of data_combine) {
-      //   if (!unique_x_dict[x_val]) {
-      //     unique_x_dict[x_val] = true;
-      //     filtered_combined.push([x_val, y_val]);
-      //   }
-      // }
+        if (filteredData.length === 0) {
+            console.log(`No rows found with idtag ${bus}.`);
+            return;
+        }
+        // for(const state of )
+        const X = tf.tensor2d(filteredData.map((row) => [row.soc]), [filteredData.length, 1]);
+        const y = tf.tensor2d(filteredData.map((row) => [row.accDiff]/60),  [filteredData.length, 1]);
+        const y_s = tf.tensor2d(filteredData.map((row) => [row.scaled]/60),  [filteredData.length, 1]);
 
-      // const sorted_combined = filtered_combined.sort((a, b) => a[0] - b[0]);
+        const { mean: X_mean, variance: X_variance } = tf.moments(X, 0);
+        const X_normalized = tf.div(tf.sub(X, X_mean), tf.sqrt(X_variance.add(1e-8)));
 
-      // const [sorted_soc, sorted_y_pred] = sorted_combined.reduce(
-      //   ([soc, y_pred], [x_val, y_val]) => [[...soc, x_val], [...y_pred, y_val]],
-      //   [[], []]
-      // );
+        const { mean: y_mean, variance: y_variance } = tf.moments(y_s, 0);
+        const y_s_normalized = tf.div(tf.sub(y_s, y_mean), tf.sqrt(y_variance.add(1e-8)));
 
-      // console.log(`bus: ${bus}, sorted_soc: ${sorted_soc}, sorted_y_pred: ${sorted_y_pred}`);
+        const splitRatio = 0.8;
+        const totalSamples = X_normalized.shape[0];
+        const trainSize = Math.floor(splitRatio * totalSamples);
 
-      // const r2_score = model.evaluate(scaler.apply(X_test), scaler.apply(y_test)).dataSync()[0];
-      // console.log(`R² Score: ${r2_score}`);
+        const indices = tf.util.createShuffledIndices(totalSamples);
+        const trainIndices = indices.slice(0, trainSize);
+        const testIndices = indices.slice(trainSize);
 
-      // const result_sorted_soc = sorted_soc.join(',');
-      // const result_sorted_y_pred = sorted_y_pred.join(',');
+        const trainIndicesArray = Array.from(trainIndices);
+        const testIndicesArray = Array.from(testIndices);
 
-      // const new_tuple = [bus, result_sorted_soc, result_sorted_y_pred, r2_score];
-      // console.log(new_tuple);
+
+        const X_train = tf.gather(X_normalized, trainIndicesArray);
+        const y_train = tf.gather(y_s_normalized, trainIndicesArray);
+        const X_test = tf.gather(X_normalized, testIndicesArray);
+        const y_test = tf.gather(y_s, testIndicesArray);
+
+        const model = tf.sequential({
+            layers: [
+            tf.layers.dense({inputShape: [X_train.shape[1]], units: 100, activation: 'relu'}),
+            tf.layers.dense({units: 50, activation: 'relu'}),
+            tf.layers.dense({units: 25, activation: 'relu'}),
+            tf.layers.dense({units: 1}),
+            ]
+        });
+
+        model.summary();
+
+        model.compile({
+            loss: 'meanSquaredError', 
+            optimizer: tf.train.adam(0.01), 
+            metrics: ['mse']
+        });
+
+        const history = await model.fit(X_train, y_train, {
+            epochs: 20,
+            batchSize: 32,
+            shuffle: true,
+        });
+
+        // console.log('Training History:', history.history);
+
+        // const historyDataFrame = tf.data.array(history.history);
+
+        // const modelSavePath = path.resolve('./models');
+
+        // if (!fs.existsSync(modelSavePath)) {
+        //     fs.mkdirSync(modelSavePath, { recursive: true });
+        //   }
+
+        // const modelFilePath = path.join(modelSavePath, 'model');
+
+        // await model.save(`file://${modelFilePath}`);
+
+        const predictions_normalized = model.predict(X_test);
+        const predictions = tf.mul(predictions_normalized, tf.sqrt(y_variance.add(1e-8))).add(y_mean);
+
+        const predictedValues = await predictions.array();
+
+        const mse = tf.metrics.meanSquaredError(y_test, predictedValues).dataSync()[0];
+        const rSquared = calculateRSquared(y_test, predictedValues);
+
+        console.log(`Bus: ${bus}, Mean Squared Error (MSE): ${mse}, R-squared: ${rSquared}`);
+
+        let XTest;
+        if (X_test && X_mean && X_variance) {
+            const X_test_denormalized = tf.mul(X_test, tf.sqrt(X_variance.add(1e-8))).add(X_mean);
+            XTest = await X_test_denormalized.array();
+        } else {
+            XTest = X_test;
+        }
+
+        const pairs = XTest.map((item, index) => [item[0], predictedValues[index][0]]);
+
+        // Sort the pairs by XTest
+        pairs.sort((a, b) => a[0] - b[0]);
+
+        const pairsMap = new Map(pairs);
+        const uniquePairs = Array.from(pairsMap.entries());
+
+        // Separate the sorted pairs for the return
+        const sortedXTest = uniquePairs.map(pair => pair[0]);
+        const sortedPredictions = uniquePairs.map(pair => pair[1]);
+
+        const XTest_sort = sortedXTest.map(item => item).join(',');
+
+        const predictedValuesString = sortedPredictions.map(item => item).join(',');
+
+        xmlData.buses.push({
+            idtag: bus,
+            X: XTest_sort,
+            y: predictedValuesString,
+            score:rSquared
+        });
+
     // }
-  } finally {
-    await mssql.close();
-  }
+
+    const builder = new Builder();
+    const xml = builder.buildObject(xmlData);
+
+    fs.writeFileSync('./predictions.xml', xml);
+
 }
 
+training()
 
-async function timeRange(bus, start, end) {
-  try {
-    await mssql.connect(config);
-    const selectQuery = `SELECT * FROM dbo.predicted_graf where idtag='${bus}'`;
-    const result = await mssql.query(selectQuery);
-    const df = result.recordset;
+async function readingFromXml(bus, path) {
+    const xmlData = fs.readFileSync(path, 'utf-8');
 
-    const x = df[0].xAxis.split(',').map(parseFloat);
-    const y = df[0].yAxis.split(',').map(parseFloat);
+    let targetData;
 
-    const interpFunc = new CubicSpline(x, y);
+    parseString(xmlData, { explicitArray: false }, (err, result) => {
+        if (err) {
+            console.error('Error parsing XML:', err);
+            return;
+        }
+        // console.log(result)
+        const buses = result.buses;
+        const idtags = Array.isArray(buses.idtag) ? buses.idtag : [buses.idtag];
+        const Xs = Array.isArray(buses.X) ? buses.X : [buses.X];
+        const ys = Array.isArray(buses.y) ? buses.y : [buses.y];
+        const scores = Array.isArray(buses.score) ? buses.score : [buses.score];
 
-    // Plotting using Plotly
-    const trace = {
-      x: x,
-      y: y,
-      mode: 'markers',
-      type: 'scatter',
-    };
+        const busIndex = idtags.indexOf(bus);
 
-    const layout = {
-      title: `Bus: ${bus},   R² Score: ${df[0].r2_score}`,
-    };
+        if (busIndex !== -1) {
 
-    const figure = { data: [trace], layout: layout };
+        const X = Xs[busIndex].split(',').map(Number);
+        const y = ys[busIndex].split(',').map(Number);
 
-    plotly.plot(figure, { filename: 'scatter-plot' }, (err, msg) => {
-      if (err) throw err;
-      console.log(msg);
+        targetData = {
+            idtag: idtags[busIndex],
+            X: X,
+            y: y,
+            score: scores[busIndex],
+        };
+
+        // console.log('Data for idtag', bus, ':', targetData);
+        } else {
+            console.log('idtag', bus, 'not found in the XML data.');
+        }
     });
-
-    const result1 = interpFunc(parseFloat(end)) - interpFunc(parseFloat(start));
-    const resultMinutes = round(result1, 1);
-    console.log(`for bus: ${bus}, the predicted time: ${resultMinutes} minutes`);
-    return `for bus: ${bus}, the predicted time: ${resultMinutes} minutes`;
-  } finally {
-    await mssql.close();
-  }
+    return targetData;
 }
 
-function round(value, decimals) {
-  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+function plotGraph(x, y){
+    console.log(x,y);
+    plot([{
+        x: x,
+        y: y,
+        type: 'scatter',
+        mode: 'markers',
+        marker: { color: 'black' },
+        name: 'plots'
+    }, {
+        x: x,
+        y: y,
+        name: 'line'
+    }]);
 }
 
-async function main() {
-  await predictChargeTime('20231101', '20231230');
-  // const result = await timeRange("14:1F:BA:10:C6:94", "40", "60");
-  // console.log(result);
+
+
+
+async function timeRange(bus, start, end, path) {
+
+    const result = await readingFromXml(bus, path);
+
+    console.log(result)
+    if (!result) {
+        console.log(`No data found for idtag ${bus}.`);
+        return;
+    }
+
+    const x = result.X;
+    const y = result.y;
+    const r2Score = result.score;
+
+    const interpFunc =  new Spline(x, y);
+    const predictedTime = interpFunc.at(end) - interpFunc.at(start);
+    const roundedPredictedTime = Math.round(predictedTime * 10) / 10;
+
+    console.log(`For bus ${bus}, the predicted time: ${roundedPredictedTime} minutes`);
+    console.log(`R² Score: ${r2Score}`);
+
+    // Plot the graph if needed
+    // plotGraph(x, y, interpFunc);
+
+    return {
+        bus,
+        predictedTime: roundedPredictedTime,
+        r2Score,
+    };
 }
 
-main().catch((error) => console.error(error));
+
+// timeRange("14:1F:BA:10:C6:94", "50", "60", "./predictions.xml").then(result=>console.log(result))
